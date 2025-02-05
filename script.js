6415b585472b53cfbf45ff4513410124e21063e5
ritik0312 <>
2025-02-05 11:00:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function beforeSubmit() {
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log(inputdate.value);
+  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formattedDate;
+}
